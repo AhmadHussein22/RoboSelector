1cd1604658d878a0dd5a6c1cb2fb2ab578cf2db8
Ahmad Hussein <ahmadhussein222200@gmaol.com>
2020-08-21 13:58:49 +0300

    intialize project

---
+++ src/robots.js
@@ -0,0 +1,76 @@
+export const robots = [
+
+    {
+        id :1 ,
+        name: 'Ahmad Hussein',
+        username: 'Breat',
+        email: 'ahmad.hussein@gmail.com'
+    },
+    {
+        id :2,
+        name: 'Loren Sos',
+        username: 'Great',
+        email: 'loren.sos@gmail.com'
+    },
+    {
+        id :3 ,
+        name: 'Ervin kyrie',
+        username: 'Goat',
+        email: 'ervin.kyrie@gmail.com'
+    },
+    {
+        id :4 ,
+        name: 'Lebron James',
+        username: 'Breat',
+        email: 'lebron.james@gmail.com'
+    },
+    {
+        id :5 ,
+        name: 'Ahmad bob',
+        username: 'Breat',
+        email: 'bob.hu@gmail.com'
+    }, {
+        id :6 ,
+        name: 'Huzayfa Hussein',
+        username: 'Breat',
+        email: 'huzayfa.hussein@gmail.com'
+    }, {
+        id :7 ,
+        name: 'Bobby bs',
+        username: 'Breat',
+        email: 'bs.bobby@gmail.com'
+    }, {
+        id :8 ,
+        name: 'omar Hussein',
+        username: 'Breat',
+        email: 'king.hussein@gmail.com'
+    }, {
+        id :9 ,
+        name: 'Pablo',
+        username: 'Breat',
+        email: 'pablo879@gmail.com'
+    }, {
+        id :10 ,
+        name: 'tita',
+        username: 'Breat',
+        email: 'tata@gmail.com'
+    }, {
+        id :11 ,
+        name: 'khaled',
+        username: 'Breat',
+        email: 'khaled.hussein@gmail.com'
+    }, {
+        id :12 ,
+        name: 'fatima Hussein',
+        username: 'Breat',
+        email: 'fatima22.hussein@gmail.com'
+    }, {
+        id :13 ,
+        name: 'ibrahm Hussein',
+        username: 'Breat',
+        email: 'ibra1239.hussein@gmail.com'
+    },
+    
+
+    
+]
